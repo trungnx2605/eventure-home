a63ae596f96456f09abac1074681758c9fb4a588
hoang <duyhoang17204@gmail.com>
2023-08-01 09:16:39 +0700

    Change assets

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,211 @@
+export const MenuIcon = ({ width = 20, height = 13, className = "" }) => (
+  <svg
+    width={width}
+    height={height}
+    className={className}
+    viewBox="0 0 20 13"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect y="0.5" width="20" height="2" rx="1" fill="currentColor" />
+    <rect y="10.5" width="20" height="2" rx="1" fill="currentColor" />
+  </svg>
+);
+
+export const UserIcon = ({ width = 24, height = 25, className = "" }) => (
+  <svg
+    width={width}
+    height={height}
+    className={className}
+    viewBox="0 0 24 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect y="0.5" width="24" height="24" rx="12" fill="#857DB1" />
+    <path
+      d="M6 16.4998V17.8332C6 18.5696 6.59695 19.1665 7.33333 19.1665H16.6667C17.403 19.1665 18 18.5696 18 17.8332V16.4998C18 14.6589 16.5076 13.1665 14.6667 13.1665H9.33333C7.49238 13.1665 6 14.6589 6 16.4998Z"
+      fill="#EFEFEF"
+    />
+    <circle cx="12" cy="8.8335" r="3" fill="#EFEFEF" />
+  </svg>
+);
+
+export const CloseIcon = ({ width = 40, height = 40, className = "" }) => (
+  <svg
+    width={width}
+    height={height}
+    className={className}
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="40" height="40" rx="8" fill="#9E99CE" />
+    <rect
+      x="13.4141"
+      y="12.1421"
+      width="20"
+      height="2"
+      rx="1"
+      transform="rotate(45 13.4141 12.1421)"
+      fill="white"
+    />
+    <rect
+      x="12.4141"
+      y="26.1421"
+      width="20"
+      height="2"
+      rx="1"
+      transform="rotate(-45 12.4141 26.1421)"
+      fill="white"
+    />
+  </svg>
+);
+
+export const VoiceIcon = ({ width = 30, height = 30, className = "" }) => (
+  <svg
+    width={width}
+    height={height}
+    className={className}
+    viewBox="0 0 30 30"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g id="Voice" clipPath="url(#clip0_1_814)">
+      <g id="Group 424">
+        <g id="Vector">
+          <path
+            d="M2.60379 6.32221C3.07214 6.40265 3.57933 6.24206 4.07018 6.20404C14.5533 5.36504 23.9391 11.9798 26.6826 22.1411C27.0265 23.4108 27.478 23.8596 28.2378 23.6804C29.0319 23.4942 29.2456 22.7944 28.8962 21.524C25.7135 9.92866 14.5959 2.40777 2.76655 3.9016C1.06161 4.11254 0.654528 5.98342 2.60379 6.32221Z"
+            fill="url(#paint0_linear_1_814)"
+          />
+          <path
+            d="M2.60379 6.32221C3.07214 6.40265 3.57933 6.24206 4.07018 6.20404C14.5533 5.36504 23.9391 11.9798 26.6826 22.1411C27.0265 23.4108 27.478 23.8596 28.2378 23.6804C29.0319 23.4942 29.2456 22.7944 28.8962 21.524C25.7135 9.92866 14.5959 2.40777 2.76655 3.9016C1.06161 4.11254 0.654528 5.98342 2.60379 6.32221Z"
+            fill="url(#paint1_linear_1_814)"
+          />
+        </g>
+        <g id="Vector_2">
+          <path
+            d="M14.9469 13.9678C11.9961 12.164 8.83796 11.3133 5.40602 11.4754C5.19105 11.4865 4.97541 11.5031 4.76113 11.5265C4.00331 11.6093 3.52779 12.0163 3.59957 12.7905C3.67341 13.5751 4.23572 13.8663 4.98535 13.8048C7.01105 13.6349 8.9898 13.8518 10.8895 14.5795C14.8793 16.1053 17.5262 18.9315 18.9183 22.9568C18.9691 23.1033 19.011 23.2498 19.0583 23.3969C19.2903 24.1177 19.7767 24.5185 20.5241 24.2865C21.2647 24.056 21.4621 23.452 21.25 22.7237C20.5766 20.4074 19.4591 18.3262 17.801 16.5762C16.9158 15.643 15.9005 14.8339 14.9448 13.9665L14.9469 13.9678Z"
+            fill="url(#paint2_linear_1_814)"
+          />
+          <path
+            d="M14.9469 13.9678C11.9961 12.164 8.83796 11.3133 5.40602 11.4754C5.19105 11.4865 4.97541 11.5031 4.76113 11.5265C4.00331 11.6093 3.52779 12.0163 3.59957 12.7905C3.67341 13.5751 4.23572 13.8663 4.98535 13.8048C7.01105 13.6349 8.9898 13.8518 10.8895 14.5795C14.8793 16.1053 17.5262 18.9315 18.9183 22.9568C18.9691 23.1033 19.011 23.2498 19.0583 23.3969C19.2903 24.1177 19.7767 24.5185 20.5241 24.2865C21.2647 24.056 21.4621 23.452 21.25 22.7237C20.5766 20.4074 19.4591 18.3262 17.801 16.5762C16.9158 15.643 15.9005 14.8339 14.9448 13.9665L14.9469 13.9678Z"
+            fill="url(#paint3_linear_1_814)"
+          />
+        </g>
+        <g id="Vector_3">
+          <path
+            d="M10.6413 20.5755C9.46311 19.7795 8.20724 19.4477 6.83666 19.5199C6.08086 19.5595 5.57177 19.9324 5.57851 20.7019C5.58663 21.4872 6.12227 21.8318 6.88151 21.8093C8.89018 21.7496 10.2204 22.6647 10.8894 24.5599C11.1295 25.2416 11.6302 25.5808 12.3304 25.3619C13.0847 25.1286 13.2725 24.5301 13.0468 23.8045C12.6178 22.4308 11.8071 21.3367 10.6426 20.5735L10.6413 20.5755Z"
+            fill="url(#paint4_linear_1_814)"
+          />
+          <path
+            d="M10.6413 20.5755C9.46311 19.7795 8.20724 19.4477 6.83666 19.5199C6.08086 19.5595 5.57177 19.9324 5.57851 20.7019C5.58663 21.4872 6.12227 21.8318 6.88151 21.8093C8.89018 21.7496 10.2204 22.6647 10.8894 24.5599C11.1295 25.2416 11.6302 25.5808 12.3304 25.3619C13.0847 25.1286 13.2725 24.5301 13.0468 23.8045C12.6178 22.4308 11.8071 21.3367 10.6426 20.5735L10.6413 20.5755Z"
+            fill="url(#paint5_linear_1_814)"
+          />
+        </g>
+      </g>
+    </g>
+    <defs>
+      <linearGradient
+        id="paint0_linear_1_814"
+        x1="16.9119"
+        y1="3.22319"
+        x2="3.12149"
+        y2="25.0797"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#7684FF" />
+        <stop offset="0.807292" stopColor="#FF98E2" />
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear_1_814"
+        x1="9.05585"
+        y1="19.3208"
+        x2="20.8848"
+        y2="7.49189"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0.0993526" stopColor="#7684FF" />
+        <stop offset="1" stopColor="#FF98E2" />
+      </linearGradient>
+      <linearGradient
+        id="paint2_linear_1_814"
+        x1="18.6632"
+        y1="7.41704"
+        x2="7.14142"
+        y2="24.7278"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#7684FF" />
+        <stop offset="0.807292" stopColor="#FF98E2" />
+      </linearGradient>
+      <linearGradient
+        id="paint3_linear_1_814"
+        x1="10.3704"
+        y1="20.6355"
+        x2="14.3134"
+        y2="16.6925"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0.0993526" stopColor="#7684FF" />
+        <stop offset="1" stopColor="#FF98E2" />
+      </linearGradient>
+      <linearGradient
+        id="paint4_linear_1_814"
+        x1="11.6799"
+        y1="17.9076"
+        x2="8.36385"
+        y2="24.027"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#7684FF" />
+        <stop offset="0.807292" stopColor="#FF98E2" />
+      </linearGradient>
+      <linearGradient
+        id="paint5_linear_1_814"
+        x1="9.05641"
+        y1="23.921"
+        x2="11.6851"
+        y2="20.6352"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0.0993526" stopColor="#7684FF" />
+        <stop offset="1" stopColor="#FF98E2" />
+      </linearGradient>
+      <clipPath id="clip0_1_814">
+        <rect width="30" height="30" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export const ArrowLeftIcon = ({ width = 19, height = 16, className = "" }) => (
+  <svg
+    width={width}
+    height={height}
+    className={className}
+    viewBox="0 0 19 16"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8.70711 13.7929C9.09763 14.1834 9.09763 14.8166 8.70711 15.2071C8.31658 15.5976 7.68342 15.5976 7.29289 15.2071L1.5 9.41421C0.718954 8.63317 0.718952 7.36684 1.5 6.58579L7.29289 0.792894C7.68342 0.402369 8.31658 0.402369 8.70711 0.792894C9.09763 1.18342 9.09763 1.81658 8.70711 2.20711L3.91422 7H18C18.5523 7 19 7.44772 19 8C19 8.55229 18.5523 9 18 9H3.91421L8.70711 13.7929Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export const ArrowRightIcon = ({ width = 19, height = 16, className = "" }) => (
+  <svg
+    width={width}
+    height={height}
+    className={className}
+    viewBox="0 0 19 16"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M10.2929 13.7929C9.90237 14.1834 9.90237 14.8166 10.2929 15.2071C10.6834 15.5976 11.3166 15.5976 11.7071 15.2071L17.5 9.41421C18.281 8.63317 18.281 7.36684 17.5 6.58579L11.7071 0.792894C11.3166 0.402369 10.6834 0.402369 10.2929 0.792894C9.90237 1.18342 9.90237 1.81658 10.2929 2.20711L15.0858 7H1C0.447715 7 0 7.44772 0 8C0 8.55229 0.447715 9 1 9H15.0858L10.2929 13.7929Z"
+      fill="currentColor"
+    />
+  </svg>
+);
